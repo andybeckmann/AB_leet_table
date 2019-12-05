93edc153d39372c9bb6f5d6ad59fc16ccb052add
Andy Beckmann <abec19@gmail.com>
2019-12-05 15:37:16 -0500

    Move code inside a function

--- js/leet_table.js
+++ js/leet_table.js
@@ -1,23 +1,34 @@
 (function(){
 
-	var data = document.getElementsByClassName("data"),
-		headers = document.getElementsByClassName("header");
+	"use strict";
 
-	var headerIndex = 0;
+	function leet_table(id) {
 
-	for (var i=0; i<data.length; i++) {
+		// Set vars for data and headers
+		var data = document.getElementById(id).getElementsByTagName("td"),
+			headers = document.getElementById(id).getElementsByTagName("th");
 
-		// Reset headerIndex when the current index reaches the last header
-		if (headerIndex >= headers.length) {
-			headerIndex = 0;
+		// Initialize headerIndex var
+		var headerIndex = 0;
+
+		// Loop through data
+		for (var i=0; i<data.length; i++) {
+
+			// Reset headerIndex when the current index reaches the last header
+			if (headerIndex >= headers.length) {
+				headerIndex = 0;
+			}
+
+			// Set data-title attribute on current data item using the textContent from the table headers
+			document.getElementsByClassName("data")[i].setAttribute('data-title', document.getElementsByClassName("header")[headerIndex].textContent);
+
+			// Increase headerIndex
+			headerIndex++;
+
 		}
-
-		// Set data-title attribute on current data item using the textContent from the table headers
-		document.getElementsByClassName("data")[i].setAttribute('data-title', document.getElementsByClassName("header")[headerIndex].textContent);
-
-		// Increase headerIndex
-		headerIndex++;
-
 	}
+
+	// Example
+	leet_table("leet_table");
 
 })();
