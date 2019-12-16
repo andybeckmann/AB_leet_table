ac0c31e3096f402ac13f73ec31fb65e92e981d8d
Andy Beckmann <abec19@gmail.com>
2019-12-16 14:28:39 -0500

    Switch to dart sass from node sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,10 @@ module.exports = function(grunt) {
 
         // -- SASS Compilation
 
-        sass:{
-            dist:{
+        'dart-sass': {
+            target: {
                 files: {
-                    "css/main.css": "scss/import.scss"
+                    'css/main.css': 'scss/main.scss'
                 }
             }
         },
@@ -51,5 +51,5 @@ module.exports = function(grunt) {
 
     require('load-grunt-tasks')(grunt);
  
-    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
+    grunt.registerTask('default', ['dart-sass', 'cssmin', 'watch']);
 };
