b61e96075b6c265eaac778903f28cf56999a77be
Andy Beckmann <abec19@gmail.com>
2019-12-16 14:30:12 -0500

    Move example scripts to demo.html

--- js/leet_table.js
+++ js/leet_table.js
@@ -1,34 +1,34 @@
-(function(){
+/** 
+ * AB_leet_table
+ * version: 1.0.0
+ * 
+ * leetTable() sets data-title attribute for data items containing header text
+ *
+ * @param {string} id - Element ID
+ */
 
-	"use strict";
+function leetTable(id) {
 
-	function leet_table(id) {
+	// Set vars for data and headers
+	var data = document.getElementById(id).getElementsByTagName("td"),
+		headers = document.getElementById(id).getElementsByTagName("th");
 
-		// Set vars for data and headers
-		var data = document.getElementById(id).getElementsByTagName("td"),
-			headers = document.getElementById(id).getElementsByTagName("th");
+	// Initialize headerIndex var
+	var headerIndex = 0;
 
-		// Initialize headerIndex var
-		var headerIndex = 0;
+	// Loop through data
+	for (var i=0; i<data.length; i++) {
 
-		// Loop through data
-		for (var i=0; i<data.length; i++) {
-
-			// Reset headerIndex when the current index reaches the last header
-			if (headerIndex >= headers.length) {
-				headerIndex = 0;
-			}
-
-			// Set data-title attribute on current data item using the textContent from the table headers
-			data[i].setAttribute('data-title', headers[headerIndex].textContent);
-
-			// Increase headerIndex
-			headerIndex++;
-
+		// Reset headerIndex when the current index reaches the last header
+		if (headerIndex >= headers.length) {
+			headerIndex = 0;
 		}
+
+		// Set data-title attribute on current data item using the textContent from the table headers
+		data[i].setAttribute('data-title', headers[headerIndex].textContent);
+
+		// Increase headerIndex
+		headerIndex++;
+
 	}
-
-	// Example
-	leet_table("leet_table");
-
-})();
+}
